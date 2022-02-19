30df7a9cfd22405f59fa4d94abb6c05a9eeb30cc
Madushan Nipuna Shantharathna <66712958+Madushan97@users.noreply.github.com>
2022-02-19 21:08:21 +0530

    Action Parameter

---
+++ MovieVD/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieVD.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    // /movies/random
+    // here movie is a controller and random is an action
+}
