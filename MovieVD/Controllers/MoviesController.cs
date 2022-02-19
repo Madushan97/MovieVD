1ad1c1e4117cc0b45c42d0adbc97e725008bb0b7
Madushan Nipuna Shantharathna <66712958+Madushan97@users.noreply.github.com>
2022-02-20 00:13:14 +0530

    Attribute routing

--- MovieVD/Controllers/MoviesController.cs
+++ MovieVD/Controllers/MoviesController.cs
@@ -12,25 +12,18 @@ namespace MovieVD.Controllers
         // GET: Movies
         public ActionResult Random()
         {
-            var movie = new Movie() { Name = "Shrek!"};
-            return View(movie);           
+            return View();
         }
 
-        //creating new action
-        public ActionResult Edit(int id)
-        {
-            return Content("id= " + id);
+        [Route("movies/released/{year}/{month:regax(\\d{4}):range(1, 12)}")]
+        
+        public ActionResult ByReleasedDate(int year, int month)
+            {
+            return Content(year + "/" + month);
         }
-        // movies
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
+                 
+        
 
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
+        
     }
 }
